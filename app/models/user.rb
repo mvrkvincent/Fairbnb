04403f176e3df8611c912fc0f7f83edac0b85381
Mark Rodriguez <mrkxvncnt@gmail.com>
2019-03-26 17:23:40 -0400

    Resolve additional heroku error

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :password_digest, :session_token, presence: true
-  validates :password, length: { minimum: 6; allow_nil: true }
+  validates :password, length: { minimum: 6, allow_nil: true }
 
   attr_reader :password
 
