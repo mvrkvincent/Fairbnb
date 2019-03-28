855852d59640745d9aa88e2e3fa9d9911a8f9193
Mark Rodriguez <mrkxvncnt@gmail.com>
2019-03-28 14:09:18 -0400

    Style search bar

--- frontend/fairbnb.jsx
+++ frontend/fairbnb.jsx
@@ -17,8 +17,6 @@ document.addEventListener('DOMContentLoaded', () => {
     store = configureStore();
   }
 
-  window.getState = store.getState;
-
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 
