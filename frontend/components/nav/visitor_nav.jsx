f7960e865bfe693a8feba400ec19943f15e27203
Mark Rodriguez <mrkxvncnt@gmail.com>
2019-03-29 19:52:27 -0400

    Implement handling of various size displays

--- frontend/components/nav/visitor_nav.jsx
+++ frontend/components/nav/visitor_nav.jsx
@@ -5,7 +5,7 @@ class VisitorNav extends React.Component {
   render() {
 
     return (
-      <div>
+      <div className="nav-buttons">
         <button onClick={this.props.demo}>Demo member</button>
         <button onClick={this.props.about}>About</button>
         <button onClick={this.props.signup}>Sign up</button>
